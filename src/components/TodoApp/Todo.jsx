e4529c65c67d59eff88d22001359ac066e79d655
Praveen Singh <praveensingh.199@gmail.com>
2025-12-20 01:55:28 +0530

    data & time added with useEffect

--- src/components/TodoApp/Todo.jsx
+++ src/components/TodoApp/Todo.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MdCheck, MdDeleteForever } from "react-icons/md";
 import "./Todo.css";
 
 export const Todo = () => {
   const [inputValue, setInputValue] = useState("");
   const [task, setTak] = useState([]);
+  const [dateTime, setDataTime] = useState("");
 
   const handleInputChange = (value) => {
     setInputValue(value);
@@ -20,43 +21,59 @@ export const Todo = () => {
     setTak((prevTask) => [...prevTask, inputValue]);
     setInputValue("");
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const formattedDate = now.toLocaleDateString();
+      const formattedTime = now.toLocaleTimeString();
+      setDataTime(`${formattedDate} - ${formattedTime}`);
+    }, 1000);
+    return () => clearInterval(interval)
+  }, []);
+
   return (
     <>
-      <section className="todo-container">
-        <header>
-          <h1>Todo List</h1>
-        </header>
+      {/* <section className="todo-container"> */}
+      <header>
+        <h1>Todo List</h1>
+        <h2 className="date-time">{dateTime}</h2>
+      </header>
 
-        <section className="form">
-          <form onSubmit={handleFormSubmit}>
-            <div>
-              <input
-                type="text"
-                className="todo-input"
-                value={inputValue}
-                onChange={(event) => handleInputChange(event.target.value)}
-              />
-            </div>
+      <section className="form">
+        <form onSubmit={handleFormSubmit}>
+          <div>
+            <input
+              type="text"
+              className="todo-input"
+              value={inputValue}
+              onChange={(event) => handleInputChange(event.target.value)}
+            />
+          </div>
 
-            <div>
-              <button type="submit" className="todo-btn">
-                Add Task
-              </button>
-            </div>
-          </form>
-        </section>
-        <section className="myUnorderedList">
-          <ul>
-            {task.map((item, index) => {
-              <li ley="index">
-                <span>{item}</span>
-                <button className="check-btn"><MdCheck /></button>
-                <button className="delete-btn"><MdDeleteForever /></button>
-              </li>;
-            })}
-          </ul>
-        </section>
+          <div>
+            <button type="submit" className="todo-btn">
+              Add Task
+            </button>
+          </div>
+        </form>
       </section>
+      <section className="myUnorderedList">
+        <ul>
+          {task.map((item, index) => (
+              <li key={index}>
+                <span>{item}</span>
+                <button className="check-btn">
+                  <MdCheck />
+                </button>
+                <button className="delete-btn">
+                  <MdDeleteForever />
+                </button>
+              </li>
+          ))}
+        </ul>
+      </section>
+      {/* </section> */}
     </>
   );
 };
